b85bd46f1f7c2a9a1f7963a5900a6ff5312fc4d8
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-23 00:59:03 +0100

    Views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def api_home(request, *arg, **kwargs):
+    return JsonResponse({'message':'Hi there BSM'})
