d04fbeec08881aea5cdf29b2dbd3c7f6a0253aed
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-23 01:15:14 +0100

    api views

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,4 +2,5 @@ from django.http import JsonResponse
 
 def api_home(request, *arg, **kwargs):
     body = request.body
+    print(body)
     return JsonResponse({'message':'Hi there BSM'})
--- py_client/basics.py
+++ py_client/basics.py
@@ -2,5 +2,6 @@ import requests
 endpoint="http://127.0.0.1:8000/api/"
 
 get_response = requests.get(endpoint, params={'abc':123}, json={"query":"Hello world"})
+# get_response = requests.get(endpoint, )
 
 print(get_response.json())
