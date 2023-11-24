2944b9d52c1655017e80bf497327271ec215f587
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-24 10:57:05 +0100

    products model to Dict

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,5 @@
 import json
+
 from django.forms.models import model_to_dict
 
 from rest_framework.response import Response
@@ -7,9 +8,9 @@ from rest_framework.decorators import api_view
 from products.models import Product
 
 @api_view(['GET'])
-def api_home(request, *arg, **kwargs):
+def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by('?').first()
     data ={}
     if model_data:
         data = model_to_dict(model_data)
-    return Response(data) 
+    return Response(data)
