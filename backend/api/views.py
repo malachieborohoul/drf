73d1842bddd198eb6e9fd27c8a0e8cc3ed5ebfc3
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-24 01:13:51 +0100

    api views

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,12 +3,11 @@ from django.http import JsonResponse
 
 def api_home(request, *arg, **kwargs):
     body = request.body
-    data={}
+    data ={}
     try:
         data = json.loads(body)
     except:
         pass
+
     print(data)
-    data['headers']=request.headers
-    data['conetent_type']=request.content_type
-    return JsonResponse(data) 
+    return JsonResponse(data)
