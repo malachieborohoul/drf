84ea4ef31c54b7b8169e07cc142376b9fcd3d5ae
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-24 10:27:51 +0100

    products model to Dict

--- py_client/basics.py
+++ py_client/basics.py
@@ -5,3 +5,20 @@ get_response = requests.get(endpoint, params={'abc':123}, json={"query":"Hello w
 # get_response = requests.get(endpoint, )
 
 print(get_response.json()) 
+
+
+
+# import json
+# from django.forms.models import model_to_dict
+# # from django.http import JsonResponse, HttpResponse
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+# from products.models import Product
+
+# @api_view(["GET"])
+# def api_home(request, *arg, **kwargs):
+#     model_data = Product.objects.all().order_by('?').first()
+#     data={}
+#     if model_data:
+#         data = model_to_dict(model_data)
+#     return Response(data)  
