91a4359e04d47040fae39d14618441d0e040e185
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-24 02:21:31 +0100

    products model to Dict

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,12 +1,13 @@
 import json
 from django.forms.models import model_to_dict
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from products.models import Product
 def api_home(request, *arg, **kwargs):
     model_data = Product.objects.all().order_by('?').first()
     data ={}
     if model_data:
-        data = model_to_dict(model_data, fields=['id', 'title'])
-    return JsonResponse(data)
+        data = model_to_dict(model_data, )
+        json_data_str =json.dumps(data)
+    return HttpResponse(json_data_str, headers={"content-type":"application/json"})
     
 
