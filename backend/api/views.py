6cb5f93d1a82c6e634a9f2022d010653094ad9a7
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-24 13:48:36 +0100

    products serializers

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,15 +1,16 @@
-from django.forms.models import model_to_dict
-
 from rest_framework.response import Response
+
 from rest_framework.decorators import api_view
 
 from products.models import Product
+
 from products.serializers import ProductSerializer
 
 @api_view(['GET'])
 def api_home(request, *args, **kwargs):
     instance = Product.objects.all().order_by('?').first()
     data ={}
+
     if instance:
         data = ProductSerializer(instance).data
     return Response(data)
--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,10 +1,9 @@
 from rest_framework import serializers
 
 from .models import Product
-
-class ProductSerializer(serializers.ModelSerializer):
+class ProductSerializer (serializers.ModelSerializer):
     class Meta:
-        model=Product
+        model = Product
         fields=[
             'title',
             'content',
