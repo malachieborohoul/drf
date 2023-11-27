20d8c9b3cbcb271ef552bfae0c9f1453a6b8244a
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-27 15:45:56 +0100

    product seriamizers

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,16 +1,17 @@
 from rest_framework import serializers
 from .models import Product
 
-class ProductSerializer(serializers.ModelSerializer):
+
+class ProductSerialize(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Product
-        fields=[
+        fields =[
             'title',
             'content',
             'price',
             'sale_price',
-            'my_discount'
+            'my_discount',
         ]
 
     def get_my_discount(self, obj):
