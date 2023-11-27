3a5374c846c7c81ef5350a49ec5ed23f93e87c1d
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-27 22:09:42 +0100

    product serializer

--- py_client/detail.py
+++ py_client/detail.py
@@ -1,8 +1,8 @@
 import requests
 endpoint="http://localhost:8000/api/products/1/"
 
-get_response = requests.get(endpoint,  json={"title":"Hello world", })
-# get_response = requests.get(endpoint, )
+# get_response = requests.get(endpoint,  json={"title":"Hello world", })
+get_response = requests.get(endpoint, )
 
 print(get_response.json()) 
 
