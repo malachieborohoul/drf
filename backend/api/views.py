1d4c4b5ded3a53984a108c18ee940ae286c3cbaa
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-27 22:01:35 +0100

    product serializer

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,13 +2,19 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from products.views import Product
 from products.serializers import ProductSerializer
-@api_view(['GET'])
-def api_home(request, *args, **kwargs):
-    instance = Product.objects.all().order_by('?').first()
-    data ={}
+# @api_view(['GET'])
+# def api_home(request, *args, **kwargs):
+#     instance = Product.objects.all().order_by('?').first()
+#     data ={}
 
-    if instance:
-        data = ProductSerializer(instance).data
-    return Response(data)
+#     if instance:
+#         data = ProductSerializer(instance).data
+#     return Response(data)
 
+
+@api_view(['POST'])
+def api_home(request,*args, **kwargs):
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        return Response(serializer.data)   
     
--- py_client/detail.py
+++ py_client/detail.py
@@ -1,7 +1,7 @@
 import requests
 endpoint="http://localhost:8000/api/products/1/"
 
-get_response = requests.get(endpoint,  json={"title":"Hello world", })
+get_response = requests.post(endpoint,  json={"title":"Hello world", })
 # get_response = requests.get(endpoint, )
 
 print(get_response.json()) 
