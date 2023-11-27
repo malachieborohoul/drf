6599451e0da05af282935929638fafc93c3c2dfd
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-27 21:49:54 +0100

    product serializer

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,9 +2,13 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from products.views import Product
 from products.serializers import ProductSerializer
-@api_view("GET")
+@api_view(['GET'])
 def api_home(request, *args, **kwargs):
-    instance = Product.objects.all().order_by('?').first
+    instance = Product.objects.all().order_by('?').first()
     data ={}
 
-  
+    if instance:
+        data = ProductSerializer(instance).data
+    return Response(data)
+
+    
--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Product
 
 
-class ProductSerialize(serializers.ModelSerializer):
+class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Product
