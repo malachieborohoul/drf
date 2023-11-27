9d8562a8859703151496475d8db745107f6f3f3a
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-27 22:38:54 +0100

    ProductDetailAPIView

--- backend/products/views.py
+++ backend/products/views.py
@@ -3,7 +3,7 @@ from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
 
-# ProductDeProProductDetailAPIVie
+# ProductDeProProductDetailAPIVi
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
