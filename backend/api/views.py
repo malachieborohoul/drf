3d30bc769d1814acfb34d5f8a3c7410a13bed78f
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-27 15:04:43 +0100

    product seriamizers

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,26 +1,10 @@
-from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
-
-
-from products.models import Product
-
+from rest_framework.response import Response
+from products.views import Product
 from products.serializers import ProductSerializer
-
-
-@api_view(['POST'])
+@api_view("GET")
 def api_home(request, *args, **kwargs):
-    serializer = ProductSerializer(data=request.data)
-    if serializer.is_valid(raise_exception=True):
-        # instance = serializer.save()
-        return Response(serializer.data)
+    instance = Product.objects.all().order_by('?').first
+    data ={}
 
-# @api_view(['GET'])
-# def api_home(request, *args, **kwargs):
-#     instance = Product.objects.all().order_by('?').first()
-#     data ={} 
-
-#     if instance:
-#         data = ProductSerializer(instance).data
-#     return Response(data) 
- 
+  
--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,7 +1,5 @@
 from rest_framework import serializers
-
 from .models import Product
-
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
