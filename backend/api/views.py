138e923a8683c06aeb2a80ce7b54125d37209c83
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-28 12:38:26 +0100

    views

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,11 +2,13 @@ from rest_framework.serializers import Serializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from django.http import JsonResponse
+
 from products.models import Product
 
 from products.serializers import ProductSerializer
 
-@api_view(['GET'])
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
     instance = Product.objects.all().order_by('?').first()
     data = {}
