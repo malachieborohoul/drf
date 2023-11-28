fbbb85e5743fe0ebed352331c9b6e60e4cb262e5
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-28 09:10:39 +0100

    ProductDetailAPIView

--- backend/products/views.py
+++ backend/products/views.py
@@ -6,7 +6,7 @@ from .serializers import ProductSerializer
 # ProductDeProProductDetaiAP
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
-    serializer_class = ProductSerializer(queryset)
+    serializer_class = ProductSerializer
 
 
- 
+  
--- py_client/detail.py
+++ py_client/detail.py
@@ -2,7 +2,7 @@ import requests
 endpoint="http://localhost:8000/api/products/1/"
 
 # get_response = requests.get(endpoint,  json={"title":"Hello world", })
-get_response = requests.get(endpoint, )
+get_response = requests.get(endpoint)
 
 print(get_response.json()) 
 
