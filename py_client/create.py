0bf59375a765edf84799e1a39b773d2e4a666553
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-28 22:14:39 +0100

    CreateAPIView

--- py_client/create.py
+++ py_client/create.py
@@ -2,7 +2,8 @@ import requests
 endpoint="http://localhost:8000/api/products/"
 
 data ={
-    "title":"This field is done",
+    "title":"This field",
+    "price":20.02,
 }
 
 get_response = requests.post(endpoint, json=data)
