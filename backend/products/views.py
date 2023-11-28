862784157cc7b0ca1f4db169afc3f65c364400d2
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-28 22:56:57 +0100

    CreateAPIView

--- backend/products/views.py
+++ backend/products/views.py
@@ -11,8 +11,13 @@ class ProductCreateAPIView(generics.CreateAPIView):
 
     def perform_create(self, serializer):
         # serializer.save(user=self.request.user)
-        print(serializer.validated_data)
-        serializer.save()
+        # print(serializer.validated_data)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+
+        if content is None:
+            content =title
+        serializer.save(content=content)
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
