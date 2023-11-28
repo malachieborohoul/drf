33facad43412a4fc4c3b884e964be31105c35cf1
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-28 23:16:04 +0100

    ListAPIView

--- backend/products/views.py
+++ backend/products/views.py
@@ -4,7 +4,7 @@ from .models import Product
 
 from .serializers import ProductSerializer
 
-# ProductListCreateA
+# ProductListCreate
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
