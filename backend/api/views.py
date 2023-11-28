5b10b3b1d1e71aca41bf9e486397997a3aee04ca
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-28 10:21:52 +0100

    models products

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,10 @@
 from rest_framework.serializers import Serializer
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from products.models import Product
 
 @api_view(['GET'])
 def api_home(request, *args, **kwargs):
-    instan
+    instance = Product.objects.all().order_by('?').first()
+    
