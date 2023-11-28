3ada422f31a10257e6ec0f7ed62ac043ae182403
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-28 15:01:46 +0100

    api

--- py_client/basics.py
+++ py_client/basics.py
@@ -1,5 +1,5 @@
 import requests
-endpoint="http://localhost:8000/api/"
+endpoint="http://localhost:8000"
 
 # get_response = requests.post(endpoint,  json={"title":"Hello world", })
 get_response = requests.get(endpoint)
