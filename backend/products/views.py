f3b5702379a7fa3b95e9ce8a324afa491b6c0a08
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-29 14:24:17 +0100

    UpdateAPIView

--- backend/products/views.py
+++ backend/products/views.py
@@ -11,49 +11,57 @@ from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
 
 # 
-# class ProductListCreateAPIView(generics.ListCreateAPIView):
-#     queryset = Product.objects.all()
-#     serializer_class = ProductSerializer
+class ProductListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
 
-#     def perform_create(self, serializer):
-#         # serializer.save(user=self.request.user)
-#         # print(serializer.validated_data)
-#         title = serializer.validated_data.get('title')
-#         content = serializer.validated_data.get('content') or None
+    def perform_create(self, serializer):
+        # serializer.save(user=self.request.user)
+        # print(serializer.validated_data)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
 
-#         if content is None:
-#             content =title
-#         serializer.save(content=content)
+        if content is None:
+            content =title
+        serializer.save(content=content)
 
-# class ProductDetailAPIView(generics.RetrieveAPIView):
-#     queryset = Product.objects.all()
-#     serializer_class = ProductSerializer
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
 
-@api_view(["GET", "POST"])
-def product_alt_view(request, pk=None, *args, **kwargs):
-    method = request.method
 
-    if method=="GET":
-        if pk is not None:
-            obj = get_object_or_404(Product, pk=pk)
-            data = ProductSerializer(obj, many=False).data
-            return Response(data)
-        else:
-            queryset = Product.objects.all()
-            data = ProductSerializer(queryset, many=True).data
-            return Response(data)
+class ProductUpdateAPIView(generics.UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def perform_update(self, serializer):
+
+# @api_view(["GET", "POST"])
+# def product_alt_view(request, pk=None, *args, **kwargs):
+#     method = request.method
+
+#     if method=="GET":
+#         if pk is not None:
+#             obj = get_object_or_404(Product, pk=pk)
+#             data = ProductSerializer(obj, many=False).data
+#             return Response(data)
+#         else:
+#             queryset = Product.objects.all()
+#             data = ProductSerializer(queryset, many=True).data
+#             return Response(data)
         
-    if method=="POST":
-        serializer=ProductSerializer(data=request.data)
+#     if method=="POST":
+#         serializer=ProductSerializer(data=request.data)
 
-        if serializer.is_valid(raise_exception=True ):
-            title = serializer.validated_data.get('title')
-            content = serializer.validated_data.get('content')
+#         if serializer.is_valid(raise_exception=True ):
+#             title = serializer.validated_data.get('title')
+#             content = serializer.validated_data.get('content')
 
-            if content is None:
-                content=title
-            serializer.save(content=content)
-            return Response(serializer.data)
+#             if content is None:
+#                 content=title
+#             serializer.save(content=content)
+#             return Response(serializer.data)
 
 
 
