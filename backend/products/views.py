b52a787052e5642831af590d00b109908e301fd8
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-29 12:44:08 +0100

    Function Based Views

--- backend/products/views.py
+++ backend/products/views.py
@@ -11,48 +11,42 @@ from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
 
 # 
-class ProductListCreateAPIView(generics.ListCreateAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+# class ProductListCreateAPIView(generics.ListCreateAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
 
-    def perform_create(self, serializer):
-        # serializer.save(user=self.request.user)
-        # print(serializer.validated_data)
-        title = serializer.validated_data.get('title')
-        content = serializer.validated_data.get('content') or None
+#     def perform_create(self, serializer):
+#         # serializer.save(user=self.request.user)
+#         # print(serializer.validated_data)
+#         title = serializer.validated_data.get('title')
+#         content = serializer.validated_data.get('content') or None
 
-        if content is None:
-            content =title
-        serializer.save(content=content)
+#         if content is None:
+#             content =title
+#         serializer.save(content=content)
 
-class ProductDetailAPIView(generics.RetrieveAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+# class ProductDetailAPIView(generics.RetrieveAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
 
-@api_view(['GET', 'POST'])
-def product_alt_view(request,pk=None, *args, **kwargs):
+def product_alt_view(request, pk=None, *args, **kwargs):
     method = request.method
 
     if method=="GET":
         if pk is not None:
-            obj= get_object_or_404(Product, pk=pk)
-            data = ProductSerializer(obj, many=False).data
+            obj = get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(obj, many=False)
             return Response(data)
         else:
             queryset = Product.objects.all()
-            data = ProductSerializer(queryset, many=True).data
+            data = ProductSerializer(queryset, many=True)
             return Response(data)
         
     if method=="POST":
-       serializer = ProductSerializer(data=request.data)
+        serializer=ProductSerializer(data=request.data)
 
-       if serializer.is_valid(raise_exception=True):
-            title = serializer.validated_data.get('title')
-            content = serializer.validated_data.get('content')
-            if content is None:
-                content = title
-            serializer.save(content=content)
-            return Response(serializer.data)
+        if serializer.is_valid():
+            
 
 
 
