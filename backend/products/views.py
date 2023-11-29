ed10d3913f88683e7c6ec2d97e61e62a09a2d141
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-29 22:52:36 +0100

    Mixins and Generic API View

--- backend/products/views.py
+++ backend/products/views.py
@@ -46,7 +46,7 @@ class ProductDeleteAPIView(generics.DestroyAPIView):
     def perform_destroy(self, instance):
         return super().perform_destroy(instance)      
     
-# ProductMi
+# ProductM
 
 class ProductMixinView(
     mixins.CreateModelMixin,
