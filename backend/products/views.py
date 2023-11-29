a5e9ce25182806c70d4c54f2a5a25d2579af6f06
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-29 14:28:05 +0100

    UpdateAPIView

--- backend/products/views.py
+++ backend/products/views.py
@@ -36,6 +36,10 @@ class ProductUpdateAPIView(generics.UpdateAPIView):
     lookup_field = 'pk'
 
     def perform_update(self, serializer):
+        instance = serializer.save()
+        if not instance.content:
+            instance.content = instance.title
+
 
 # @api_view(["GET", "POST"])
 # def product_alt_view(request, pk=None, *args, **kwargs):
