046699ee19a644ec3132d83a3e324507a60877d2
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-29 12:29:21 +0100

    Function Based Views

--- backend/products/views.py
+++ backend/products/views.py
@@ -29,18 +29,18 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-
+@api_view(['GET', 'POST'])
 def product_alt_view(request,pk=None, *args, **kwargs):
     method = request.method
 
     if method=="GET":
         if pk is not None:
             obj= get_object_or_404(Product, pk=pk)
-            data = ProductSerializer(obj, many=False)
+            data = ProductSerializer(obj, many=False).data
             return Response(data)
         else:
             queryset = Product.objects.all()
-            data = ProductSerializer(queryset, many=True)
+            data = ProductSerializer(queryset, many=True).data
             return Response(data)
         
     if method=="POST":
