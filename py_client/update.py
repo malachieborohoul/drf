14613226ba2ec09ae87297d3afbf5826b8033722
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-29 14:30:05 +0100

    UpdateAPIView

--- py_client/update.py
+++ py_client/update.py
@@ -1,8 +1,11 @@
 import requests
 endpoint="http://localhost:8000/api/products/2/"
-
+data ={
+    "title":"This is dope man",
+    "price":208.02,
+}
 # get_response = requests.get(endpoint,  json={"title":"Hello world", })
-get_response = requests.get(endpoint)
+get_response = requests.put(endpoint, json=data)
 
 print(get_response.json()) 
 
