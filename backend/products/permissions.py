ae2a351e1b1aa2f7f6bd36d6c336e1e712535a9e
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-30 23:26:30 +0100

    Custom Permissions

--- backend/products/permissions.py
+++ backend/products/permissions.py
@@ -2,4 +2,8 @@ from rest_framework import permissions
 
 class IsStaffEditorPermission(permissions.DjangoModelPermissions):
     def has_permission(self, request, view):
-        return True
+        user = request.user
+        if user.is_staff:
+            return True
+        print(user.get_all_permission())
+        return False
