f02f76182e606bd8b27e2e5fee21020040434511
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-30 23:09:38 +0100

    authentifications

--- backend/products/views.py
+++ backend/products/views.py
@@ -10,7 +10,7 @@ from rest_framework.decorators import api_view
 
 from django.shortcuts import get_object_or_404
 
-# permission_classe
+# permission_class
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
