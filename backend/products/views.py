dbde7fe48d8633d0974c024c27398e7789117dae
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-30 21:40:23 +0100

    Generic API View

--- backend/products/views.py
+++ backend/products/views.py
@@ -11,7 +11,21 @@ from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
 
 
-class ProductListAPIView(generics.ListCreateAPIView):
+class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content')
+
+        if content is None:
+            content = title
+        serializer.save(content=content)
+
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset= Product.objects.all()
+    serializer_class = ProductSerializer
+
+
     
