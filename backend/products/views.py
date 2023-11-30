786b6ecd8ed273f672525c1ec326347cc386f12d
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-30 22:29:36 +0100

    permissions authentifications

--- backend/products/views.py
+++ backend/products/views.py
@@ -14,6 +14,7 @@ from django.shortcuts import get_object_or_404
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    authentication_classes =[ ]
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
