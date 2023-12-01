eff2897dd6df63cdb2234bf364cc262d16de88f7
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-12-01 01:01:06 +0100

    Token Authentification

--- backend/products/views.py
+++ backend/products/views.py
@@ -15,7 +15,10 @@ from .permissions import IsStaffEditorPermission
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    authentication_classes =[authentication.SessionAuthentication]
+    authentication_classes =[
+        authentication.SessionAuthentication,
+        authentication.TokenAuthentication
+                             ]
     permission_classes = [permissions.IsAdminUser,IsStaffEditorPermission]
 
     def perform_create(self, serializer):
--- py_client/list.py
+++ py_client/list.py
@@ -1,4 +1,13 @@
 import requests
+from getpass import getpass
+auth_endpoint="http://localhost:8000/api/auth/"
+password=getpass()
+
+auth_response = requests.post(auth_endpoint, json={"username":"bsm","password":password})
+
+print(auth_response.json()) 
+
+
 endpoint="http://localhost:8000/api/products/"
 
 
