3f60b4355f103528e6d2eae6b5aeab5065c2cdce
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-12-04 00:09:47 +0100

    Views GET

--- backend/products/views.py
+++ backend/products/views.py
@@ -13,80 +13,24 @@ from django.shortcuts import get_object_or_404
 from .permissions import IsStaffEditorPermission
 
 from api.authentication import TokenAuthentication
-# ProProP
-class ProductListCreateAPIView(generics.ListCreateAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    authentication_classes =[
-        authentication.SessionAuthentication,
-        TokenAuthentication
-                             ]
-    permission_classes = [permissions.IsAdminUser,IsStaffEditorPermission]
-
-    def perform_create(self, serializer):
-        title = serializer.validated_data.get('title')
-        content = serializer.validated_data.get('content')
-
-        if content is None:
-            content = title
-        serializer.save(content=content)
-
-class ProductDetailAPIView(generics.RetrieveAPIView):
-    queryset= Product.objects.all()
-    serializer_class = ProductSerializer
-
-class ProductUpdateAPIView(generics.UpdateAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    authentication_classes=[authentication.SessionAuthentication]
-    permission_classes=[permissions.DjangoModelPermissions]
-    lookup_field = 'pk'
-
-
-    def perform_update(self, serializer):
-        instance = serializer.save()
-
-        if instance.content is None:
-            instance.content = instance.title
-
-
-class ProductDestroyAPIView(generics.DestroyAPIView):
-    queryset = Product.objects.all()
-    serializer_class= ProductSerializer
-
-    def perform_destroy(self, instance):
-        return super().perform_destroy(instance)
-
-
 
 @api_view(['GET', 'POST'])
 def product_alt_view(request, *args, **kwargs):
     method = request.method
-    if method == "GET":
+
+    if method=="GET":
         pk = kwargs.get('pk')
 
         if pk is not None:
-            obj = get_object_or_404(Product,pk=pk)
-            data = ProductSerializer(obj, many=False).data
+            obj= get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(obj, many=False)
             return Response(data)
         else:
             queryset = Product.objects.all()
-            data = ProductSerializer(queryset, many=True).data
+            data = ProductSerializer(queryset, many=True)
             return Response(data)
-    if method == "POST":
-        serializer = ProductSerializer(data=request.data)
-
-        if serializer.is_valid(raise_exception=True):
-            title = serializer.validated_data.get('title')
-            content = serializer.validated_data.get('content')
-
-            if content is None:
-                content = title
-            serializer.save()
-            return Response(serializer.data)
-
-
         
+    
 
 # # ProdProdPr
 # class ProductMixinView(
--- py_client/list.py
+++ py_client/list.py
@@ -19,3 +19,76 @@ if auth_response.status_code==200:
     print(get_response.json()) 
 
  
+
+ # ProPro
+# class ProductListCreateAPIView(generics.ListCreateAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+#     authentication_classes =[
+#         authentication.SessionAuthentication,
+#         TokenAuthentication
+#                              ]
+#     permission_classes = [permissions.IsAdminUser,IsStaffEditorPermission]
+
+#     def perform_create(self, serializer):
+#         title = serializer.validated_data.get('title')
+#         content = serializer.validated_data.get('content')
+
+#         if content is None:
+#             content = title
+#         serializer.save(content=content)
+
+# class ProductDetailAPIView(generics.RetrieveAPIView):
+#     queryset= Product.objects.all()
+#     serializer_class = ProductSerializer
+
+# class ProductUpdateAPIView(generics.UpdateAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+#     authentication_classes=[authentication.SessionAuthentication]
+#     permission_classes=[permissions.DjangoModelPermissions]
+#     lookup_field = 'pk'
+
+
+#     def perform_update(self, serializer):
+#         instance = serializer.save()
+
+#         if instance.content is None:
+#             instance.content = instance.title
+
+
+# class ProductDestroyAPIView(generics.DestroyAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class= ProductSerializer
+
+#     def perform_destroy(self, instance):
+#         return super().perform_destroy(instance)
+
+
+
+# @api_view(['GET', 'POST'])
+# def product_alt_view(request, *args, **kwargs):
+#     method = request.method
+#     if method == "GET":
+#         pk = kwargs.get('pk')
+
+#         if pk is not None:
+#             obj = get_object_or_404(Product,pk=pk)
+#             data = ProductSerializer(obj, many=False).data
+#             return Response(data)
+#         else:
+#             queryset = Product.objects.all()
+#             data = ProductSerializer(queryset, many=True).data
+#             return Response(data)
+#     if method == "POST":
+#         serializer = ProductSerializer(data=request.data)
+
+#         if serializer.is_valid(raise_exception=True):
+#             title = serializer.validated_data.get('title')
+#             content = serializer.validated_data.get('content')
+
+#             if content is None:
+#                 content = title
+#             serializer.save()
+#             return Response(serializer.data)
+
