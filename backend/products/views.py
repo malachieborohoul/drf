d05e50430e9876de5ea4ff6a481ff53fc69d21a1
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-12-04 00:18:51 +0100

    Views POST

--- backend/products/views.py
+++ backend/products/views.py
@@ -29,7 +29,19 @@ def product_alt_view(request, *args, **kwargs):
             queryset = Product.objects.all()
             data = ProductSerializer(queryset, many=True)
             return Response(data)
-        
+    
+    if method =="POST":
+        serializer = ProductSerializer(data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data.get('title')
+            content = serializer.validated_data.get('content')
+
+            if content is None:
+                content = title
+            serializer.save()
+            return Response(serializer.data)
+
     
 
 # # ProdProdPr
