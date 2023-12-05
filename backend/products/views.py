ca096070db7275441ced8ea7b0ba8357d22aaedf
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-12-05 23:10:14 +0100

    Views DestroyAPIView

--- backend/products/views.py
+++ backend/products/views.py
@@ -14,7 +14,7 @@ from .permissions import IsStaffEditorPermission
 
 from api.authentication import TokenAuthentication
 
-# ResponseSuccessResponse
+# ResponseSuccessRespons
 
 class ProductRetriveAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
